79bb659ecb5540b7b81118ecc45beeb5763750ec
Rehmat <95978634+muskan3233@users.noreply.github.com>
2023-06-23 12:59:36 +0530

    Update simple-interest.sh
    added username

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # <muskan3233>
    # Input:
    # p, principal amount
    # t, time period in years
